522815db011f89644088eb82064288d3fcd1801a
kk0195736 <kk019573611296911@gmail.com>
2018-11-19 14:25:29 +0900

    add

--- ABS/ABC085C.py
+++ ABS/ABC085C.py
@@ -1,20 +1,12 @@
 N, Y = map(int, input().split())
 
 def rec(i, j):
-    if  i == 0 and j == 0:
-        return print("True")
+    if  i == :
+        return False
     else:
-        if i == 0 and j != 0:
-            return print("-1 -1 -1")
-        else:
-            if j >= 10000:
-                
-                return rec(i-1, j-10000)
-            elif j >= 5000:
-                
-                return rec(i-1, j-5000)
-            else:
-                
-                return rec(i-1, j-1000)
+        x = y = z = 0
+        for i in range(i):
+            
+        
 
 print(rec(N, Y))
