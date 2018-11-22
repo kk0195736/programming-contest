0db7b4ec2f45eac702b51edd2a249122ed70bd2b
kk0195736 <kk019573611296911@gmail.com>
2018-11-22 23:42:13 +0900

    update money.py

--- DP/ex/money.py
+++ DP/ex/money.py
@@ -5,10 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-initial_balance = 100000  # 初期資金の設定
+initial_balance = 1000000  # 初期資金の設定
 initial_bets = 1000  # 初期掛け金の設定
 trials = 100  # 試行回数の設定
-limit = 3  # 取引制限値の設定
+limit = 100  # 取引制限値の設定
 
 payout = 1.85  # ペイアウトの設定
 
@@ -23,13 +23,15 @@ for i in range(trials):  # trails回実行する
     win_count = 0  # 勝った回数の初期化
     lose_count = 0  # 負けた回数の初期化
 
+    max_losing_count = 0
+
     while True:  # 取引を繰り返す
 
         if balance < initial_bets:  # 残高が初期掛け金を下回ったら終了する
             break
         if balance < bets:  # 残高が現在の掛け金を下回ったら終了する
             break
-        if lose_count >= 100:  # 負けた回数がn回以上のとき終了する
+        if lose_count >= 1000:  # 負けた回数がn回以上のとき終了する
             break
 
         losing_streak_count = 0  # 連敗記録の初期化
@@ -52,6 +54,9 @@ for i in range(trials):  # trails回実行する
                 break  # その日の取引の終了
 
             else:  # 負けのとき
+                if max_losing_count < losing_streak_count:
+                    max_losing_count = losing_streak_count
+
                 lose_count += 1  # 回数の加算
                 losing_streak_count += 1  # 連敗カウントの加算
 
@@ -64,8 +69,8 @@ for i in range(trials):  # trails回実行する
                 # 掛け金を次に勝ったときに勝ち分が(initial_bets * (1-payout))円となるよう調整する
                 bets = int((parallel_balance - balance)/(payout - 1))
 
-    print("result:{} win:{} 取引回数:{} 勝ち:{} 負け:{}".format(
-        balance, balance - initial_balance, win_count + lose_count, win_count, lose_count))  # 結果の出力
+    print("result:{} win:{} 取引回数:{} 勝ち:{} 負け:{} 最大連敗数:{}".format(
+        balance, balance - initial_balance, win_count + lose_count, win_count, lose_count, max_losing_count))  # 結果の出力
     result_balance = np.append(result_balance, balance)  # 残高を記録する。
     result_tarade_count = np.append(
         result_tarade_count, win_count+lose_count)  # 試行回数を記録する
